374dfc9acb0840cc6ad6522df9d505c9b584add7
Javier Galan <javier.galan@unizar.es>
2021-07-20 17:50:55 +0200

    Update launchRsync.sh

--- launchRsync.sh
+++ launchRsync.sh
@@ -5,7 +5,7 @@
 ####### A script to do rsync to a remote machine ready to be used with a cronjob
 #######
 ####### If rsync is already running it does nothing, but if it is running for more 
-####### than 2-hours it will kill the rsynnc process
+####### than 2-hours it will kill the rsync process
 #######
 ####### Usage: Replace the upper case variables at "rsync" command with the local 
 ####### and remote systems coordinates.
@@ -14,8 +14,9 @@
 ####### at /home/USER/.ssh/id_rsa.pub. In order for the remote system to know the
 ####### the public key, this must be copied at /home/REMOTE/.ssh/authorized_keys
 #######
-####### Then, edit the cronjobs using "crontab -e" command, adding the following 
-####### line in order to execute the syncronization every 5 minutes.
+####### Then, edit the cronjobs at the local system using "crontab -e" command, 
+####### adding the following line in order to execute the syncronization every 
+####### 5 minutes.
 #######
 ####### 1,6,11,16,21,26,31,36,41,46,51,56 * * * * /path/to/script/launcRsync.sh
 #######
