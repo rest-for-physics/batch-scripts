a4c05f0f11e6d146097823e7619b3f8875524d4c
Javier Galan <j.a.galan.81@gmail.com>
2022-10-13 16:55:00 +0200

    restManagerToCondor.py allows now glob pattern file input

--- condor/restManagerToCondor.py
+++ condor/restManagerToCondor.py
@@ -5,7 +5,7 @@
 # 2 - Oct- 2016
 
 import os,sys, time, commands
-import stat
+import stat, glob
 
 calrun = 0
 bckrun = 0
@@ -28,16 +28,18 @@ if narg < 2:
     print " This program launches restManager job to condor	    "
     print " (condor scripts will be created under condor/ directory)"
     print ""
-    print " The usual restManager command is : restManager --c CONFIG_FILE --f FILENAME [--n SECTION_NAME]"
+    print " The usual restManager command is : restManager --c CONFIG_FILE --f FILENAME"
     print ""
-    print " Values in brackets [] are optional"
+    print " Usage : restManagerToCondor.py -c CONFIG_FILE -f FILELIST/GLOBPATTERN "
     print ""
-    print " Usage : restManagerToCondor.py -c CONFIG_FILE -f LISTFILE [-n SECTION_NAME] "
+	print " Two options are allowed as input for the -f option "
+    print " - FILELIST. A file containing one file per line"
+	print " - GLOBPATTERN. A glob filename pattern using wild cards *?" 
     print ""
-    print " The file LISTFILE must a contain a REST file to be processed, one file per line and full path"
+    print " In all cases full path file location should be given!"
     print ""
-    print " - Options : " 
-    print " ----------- " 
+    print " - Other options : " 
+    print " ----------------- " 
     print ""
     print " -j or --jobName JOB_NAME :"
     print " JOB_NAME defines the name of scripts and output files stored under condor/"
@@ -80,20 +82,21 @@ if cfgFile == "":
     print "Please specify a RML config file list using -c flag." 
     sys.exit( 1 )
 
-if fileList == "":
-    print "Please specify a file list using -f flag." 
-    sys.exit( 1 )
-
-
-f = open(fileList,'r')
 lines = []
-for line in f.readlines():
-    if line[0] != "#":
-        lines.append( line )
-f.close()
+if (fileList != ""):
+	if( not "*" in fileList ):
+		f = open(fileList,'r')
+		for line in f.readlines():
+			if line[0] != "#":
+				lines.append( line )
+		f.close()
+	else:
+		lines = glob.glob( fileList ) 
+
+print ( "Files to process:" )
+print ( lines )
 
 cont = 0
-#!/bin/bash
 for fileToProcess in lines:
 
     scriptName = "condor/" + jobName+"_"+str(cont)
@@ -105,7 +108,7 @@ for fileToProcess in lines:
     for key in os.environ.keys(): 
         if key.find("REST") == 0:
             f.write( "export " + key + "=" + os.environ[key] +"\n" )
-        print( "export " + key + "=" + os.environ[key] +"\n" )
+     #   print( "export " + key + "=" + os.environ[key] +"\n" )
         if key.find("PATH") == 0:
             f.write( "export " + key + "=" + os.environ[key] +"\n" )
         if key.find("LD_LIBRARY_PATH") == 0:
@@ -117,12 +120,9 @@ for fileToProcess in lines:
         if key.find("PWD") == 0:
             f.write( "export " + key + "=" + os.environ[key] +"\n" )
 
-    f.write("export BL2_HOME="+ os.environ['BL2_HOME']+"\n")
-    f.write("export SCRATCH_HOME="+ os.environ['SCRATCH_HOME']+"\n")
-    f.write("export OPT_PATH="+ os.environ['OPT_PATH']+"\n")
     f.write("export USER="+ os.environ['USER']+"\n\n")
 
-    command = "restManager --c " + os.environ['PWD'] + "/" + cfgFile + " --f " + fileToProcess
+    command = os.environ['REST_PATH'] + "/bin/restManager --c " + os.environ['PWD'] + "/" + cfgFile + " --f " + fileToProcess
     if sectionName != "":
         command = command + " --n " + sectionName
     f.write(  command + "\n" )
@@ -146,7 +146,7 @@ for fileToProcess in lines:
     if onlyScripts == 0:
         print "---> Launching : " + command
 
-        condorCommand = "condor_submit " + scriptName + "_" + str(cont) + ".condor" 
+        condorCommand = "condor_submit " + scriptName + ".condor" 
         print "Condor command : " + condorCommand
 
         print "Waiting " + str(sleep) + " seconds to launch next job" 
@@ -156,4 +156,80 @@ for fileToProcess in lines:
     else:
         print "---> Produced condor script : " + str( scriptName ) + "_" + str(cont) + "_" + str( jobName ) + ".condor"
         print "---> To launch : " + command
+
+if (fileList == "" ):
+    scriptName = "condor/" + jobName
+
+    f = open( scriptName + ".sh", "w" )
+    f.write("#!/bin/bash\n")
+    #f.write("export REST_DATAPATH="+ os.environ['REST_DATAPATH']+"\n")
+
+    for key in os.environ.keys(): 
+        if key.find( "HOME") == 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find( "DATA") == 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("GDML") == 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("GEOMETRY") >= 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("REST") == 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("G4") == 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("PATH") == 0:
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("LD_LIBRARY_PATH") == 0:
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("GARFIELD_") == 0:
+            print( "export " + key + "=" + os.environ[key] +"\n" )
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("HEED_") == 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+        if key.find("PWD") == 0:
+            f.write( "export " + key + "=" + os.environ[key] +"\n" )
+
+    f.write("export USER="+ os.environ['USER']+"\n\n")
+
+    command = "restManager --c " + cfgFile
+    if sectionName != "":
+        command = command + " --n " + sectionName
+    f.write(  command + "\n" )
+    f.close()
+
+    st = os.stat( scriptName + ".sh" )
+    os.chmod( scriptName + ".sh", st.st_mode | stat.S_IEXEC)
+
+    cont = cont + 1
+
+    g = open( scriptName + ".condor", "w" )
+    g.write("Executable = " + scriptName + ".sh\n" )
+    g.write("Arguments = \n" )
+    g.write("Log = " + scriptName + ".log\n" )
+    g.write("Output = " + scriptName + ".out\n" )
+    g.write("Error = " + scriptName + ".err\n" )
+    g.write("Queue\n" )
+    g.close()
+
+    if onlyScripts == 0:
+        print "---> Launching : " + command
+
+        condorCommand = "condor_submit " + scriptName + ".condor" 
+        print "Condor command : " + condorCommand
+
+        print "Waiting " + str(sleep) + " seconds to launch next job" 
+        time.sleep(sleep)
+
+        print commands.getstatusoutput( condorCommand )
+    else:
+        print "---> Produced condor script : " + str( scriptName ) + "_" + str( jobName ) + ".condor"
+        print "---> To launch : " + command
+    
 
