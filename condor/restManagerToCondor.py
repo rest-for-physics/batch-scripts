93f505927eba2513e87d0db102933e9a4e66faf2
Javier Galan <j.a.galan.81@gmail.com>
2023-05-24 13:22:39 +0200

    restManagerToCondor.py removing  extra job being launched without notice

--- condor/restManagerToCondor.py
+++ condor/restManagerToCondor.py
@@ -231,80 +231,6 @@ def ProcessWithoutFile( ):
 			print "---> Produced condor script : " + str( scriptName ) + "_" + str(cont) + "_" + str( jobName ) + ".condor"
 			print "---> To launch : " + command
 
-	if (fileList == "" ):
-		scriptName = "condor/" + jobName
-
-		f = open( scriptName + ".sh", "w" )
-		f.write("#!/bin/bash\n")
-		#f.write("export REST_DATAPATH="+ os.environ['REST_DATAPATH']+"\n")
-
-		for key in os.environ.keys(): 
-			if key.find( "HOME") == 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find( "DATA") == 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("GDML") == 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("GEOMETRY") >= 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("REST") == 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("G4") == 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("PATH") == 0:
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("LD_LIBRARY_PATH") == 0:
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("GARFIELD_") == 0:
-				print( "export " + key + "=" + os.environ[key] +"\n" )
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("HEED_") == 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-			if key.find("PWD") == 0:
-				f.write( "export " + key + "=" + os.environ[key] +"\n" )
-
-		f.write("export USER="+ os.environ['USER']+"\n\n")
-
-		command = "restManager --c " + cfgFile
-		if sectionName != "":
-			command = command + " --n " + sectionName
-		f.write(  command + "\n" )
-		f.close()
-
-		st = os.stat( scriptName + ".sh" )
-		os.chmod( scriptName + ".sh", st.st_mode | stat.S_IEXEC)
-
-		cont = cont + 1
-
-		g = open( scriptName + ".condor", "w" )
-		g.write("Executable = " + scriptName + ".sh\n" )
-		g.write("Arguments = \n" )
-		g.write("Log = " + scriptName + ".log\n" )
-		g.write("Output = " + scriptName + ".out\n" )
-		g.write("Error = " + scriptName + ".err\n" )
-		g.write("Queue\n" )
-		g.close()
-
-		if onlyScripts == 0:
-			print "---> Launching : " + command
-
-			condorCommand = "condor_submit " + scriptName + ".condor" 
-			print "Condor command : " + condorCommand
-
-			print "Waiting " + str(sleep) + " seconds to launch next job" 
-			time.sleep(sleep)
-
-			print commands.getstatusoutput( condorCommand )
-		else:
-			print "---> Produced condor script : " + str( scriptName ) + "_" + str( jobName ) + ".condor"
-			print "---> To launch : " + command
 
 if narg < 2:
 	print ""
@@ -408,6 +334,6 @@ if (fileList != ""):
 
 	ProcessFilesInList( lines )
 else:
-	print( "Launching processing without input file" )
+	print( " ======> Launching processing without input file <======" )
 	ProcessWithoutFile( )
 
